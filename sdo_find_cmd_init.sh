bd1108fdeb7e011e98e2121e2ef78df2114354c1
shaunak chokshi <shaunakchokshi19@gmail.com>
2018-06-27 18:22:36 -0400

    Changed the find command

--- sdo_find_cmd_init.sh
+++ sdo_find_cmd_init.sh
@@ -1,2 +1,2 @@
-echo 'function find_files() { find . -name "$1"\* }' >> ~/.bashrc
-echo 'function find_all_files() { grep -rnw -e ""$1"" }' >> ~/.bashrc
+echo 'function find_files() { find . -name "$1"\*; }' >> ~/.bashrc
+echo 'function find_all_files() { grep -rnw -e ""$1""; }' >> ~/.bashrc
